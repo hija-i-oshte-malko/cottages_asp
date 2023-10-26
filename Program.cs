71c41757a05e728982bea230a4109f3dc978773c
Georgi Petrov <hailanimepenchou@gmail.com>
2023-10-26 08:32:44 +0300

    initialize project

--- Program.cs
+++ Program.cs
@@ -5,13 +5,13 @@ using Microsoft.EntityFrameworkCore;
 
 namespace cottages_asp;
 
-public class Program
-{
-	public static void Main(string[] args)
+	public class Program
 	{
-		var builder = WebApplication.CreateBuilder(args);
+		public static void Main(string[] args)
+		{
+			var builder = WebApplication.CreateBuilder(args);
 
-		// Add services to the container.
+			// Add services to the container.
 		string connStringName = "CottagesConnectionString";
 		var connectionString = builder.Configuration.GetConnectionString(connStringName)
 			?? throw new InvalidOperationException($"Connection string '{connStringName}' not found.");
@@ -28,34 +28,36 @@ public class Program
 			.AddRoles<IdentityRole<Guid>>()
 			.AddEntityFrameworkStores<CottagesDbContext>();
 
-		builder.Services.AddControllersWithViews();
-		var app = builder.Build();
+			builder.Services.AddControllersWithViews();
 
-		// Configure the HTTP request pipeline.
+			var app = builder.Build();
+
+			// Configure the HTTP request pipeline.
 		if (app.Environment.IsDevelopment())
 		{
 			app.UseMigrationsEndPoint();
 		}
 		else
-		{
-			app.UseExceptionHandler("/Home/Error");
-			// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-			app.UseHsts();
-		}
+			{
+				app.UseExceptionHandler("/Home/Error");
+				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+				app.UseHsts();
+			}
 
-		app.UseHttpsRedirection();
-		app.UseStaticFiles();
+			app.UseHttpsRedirection();
+			app.UseStaticFiles();
 
-		app.UseRouting();
+			app.UseRouting();
 
 		app.UseAuthentication();
-		app.UseAuthorization();
+			app.UseAuthorization();
 
-		app.MapControllerRoute(
-			name: "default",
-			pattern: "{controller=Home}/{action=Index}/{id?}");
+			app.MapControllerRoute(
+				name: "default",
+				pattern: "{controller=Home}/{action=Index}/{id?}");
 		app.MapRazorPages();
 
-		app.Run();
+			app.Run();
+		}
 	}
 }

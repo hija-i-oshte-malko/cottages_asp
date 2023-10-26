b0e4de87bf840f2bbeef6506c22278fb4068c80e
Georgi Petrov <hailanimepenchou@gmail.com>
2023-10-26 08:32:44 +0300

    Initialize ASP.NET (NET 6.0) MVC project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace cottages_asp.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}

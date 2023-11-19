646358ccfa822a8b32b0acc967034d9dd1b793d5
vanchi3 <ivanamircheva73@gmail.com>
2023-11-19 13:25:24 +0200

    не знам как да оправя снимките

---
+++ Controllers/SearchController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Hija.Controllers
+{
+    public class SearchController : Controller
+    {
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+    }
+}

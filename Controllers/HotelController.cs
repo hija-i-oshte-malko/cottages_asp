7fd656327372b827925cdeb2746781a3c1ec3a3b
Dan4oVeliki <139229732+Dan4oVeliki@users.noreply.github.com>
2023-12-21 01:45:02 +0200

    HoteliDetails (#28)
    * initialize project
    
    * add profile page
    
    * Add efcore packages
    
    * Convention fixes
    
    * Remove unnecessary comments
    
    * Add user secret
    
    * Fix conventions
    
    * initialize project
    
    * cool song
    
    * onui tam kopuvane stranica
    
    * moving things around
    
    * opravih tam deto gosho vika
    
    * navbar bullshit
    
    * image gallery and shit ass navbar
    
    * fix conflicts
    
    * cool song
    
    * added images
    
    * moving things around
    
    * opravih tam deto gosho vika
    
    * navbar bullshit
    
    * Fix EVERYTHING
    
    * fix conflicts
    
    ---------
    
    Co-authored-by: Georgi Petrov <hailanimepenchou@gmail.com>
    Co-authored-by: Valentina Demireva <valentinademirevall@gmail.com>

---
+++ Controllers/HotelController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace cottages_asp.Controllers
+{
+    public class HotelController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

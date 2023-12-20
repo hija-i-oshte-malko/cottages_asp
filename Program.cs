cae4a94f4b99c2b4312ea8009d12ddb01e15ec83
Georgi Petrov <hailanimepenchou@gmail.com>
2023-12-20 10:17:55 +0200

    Remove unnecessary comments

--- Program.cs
+++ Program.cs
@@ -9,22 +9,18 @@ public class Program
 	{
 		var builder = WebApplication.CreateBuilder(args);
 
-		// Add services to the container.
 		builder.Services.AddControllersWithViews();
 		builder.Services.AddDbContext<CottagesDbContext>(options =>
 		options.UseSqlServer(builder.Configuration.GetConnectionString("CottagesConnectionString")));
 
 		var app = builder.Build();
 
-		// Configure the HTTP request pipeline.
 		if (!app.Environment.IsDevelopment())
 		{
 			app.UseExceptionHandler("/Home/Error");
-			// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
 			app.UseHsts();
 		}
-		Console.WriteLine("Raw Salamander");
-		Console.WriteLine("Ba li vse taq");
+
 		app.UseHttpsRedirection();
 		app.UseStaticFiles();
 
@@ -37,8 +33,5 @@ public class Program
 			pattern: "{controller=Home}/{action=Index}/{id?}");
 
 		app.Run();
-
-
-
 	}
 }

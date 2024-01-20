0309e690da5a6c0b7ad15d0ad195c440fb16f05f
vanchi3 <ivanamircheva73@gmail.com>
2024-01-20 13:28:06 +0200

    Connected view and entity

--- Controllers/SearchController.cs
+++ Controllers/SearchController.cs
@@ -1,12 +1,25 @@
 using Microsoft.AspNetCore.Mvc;
+using cottages_asp.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using cotagges_asp.Data;
+using cottages_asp.Models.Entities;
 
 namespace cottages_asp.Controllers;
 
 public class SearchController : Controller
 {
-	[HttpGet]
-	public async Task<IActionResult> Index()
+	public CottagesDbContext _context;
+
+	public SearchController(CottagesDbContext context)
 	{
+		_context = context;
+	}
+	[HttpGet]
+	public async Task<IActionResult> Index(BuildingViewModel buildingViewModel)
+	{
+		List<Building> buildings = await _context.Buildings.ToListAsync();
+		ViewBag.OfferOptionList = new SelectList(_context.Offers, "Id", "Name");
 		return this.View();
 	}
 }
